3172620f09a604c47ab4a7faae1e44aa7c5f615f
April Park <april_park@berkeley.edu>
2025-09-16 11:26:17 -0700

    saving homework1

---
+++ homework1/homework1.py
@@ -0,0 +1,41 @@
+# File: homework1.py
+# --- Varialbes and Data Types ---#
+
+a = 10
+print(a)
+print(type(a)) # a is an integer, a whole number with no decimals
+
+b = 1.5
+print(b)
+print(type(b)) #b is a float, a real number with decimals
+
+c =3j
+print(c)
+print(type(c)) #c is a complex, consisting a real part and imaginary part
+
+d = "hello"
+print(d)
+print(type(d)) # d is a string, which is made of characters
+
+e = [1, 2, 3]
+print(e)
+print(type(e)) # e is a list of numbers
+
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print(f)
+print(type(f)) # f is a dictionary that stores data in key-value pairs
+
+g = (1, 2)
+print(g)
+print(type(g)) # g is a tuple which is an ordered, immutable collection of items
+
+h = ["apple", "banana", "strawberry"]
+print(h)
+print(type(h)) # h is a list that contains strings
+
+i = True
+print(i)
+print(type(i))# i is a bool
+
+j = None
+
